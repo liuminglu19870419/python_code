fee99f26622723f42e5860de46761fefe43dd253
mingliu <mingliu@mingliu-virtual-machine.(none)>
2014-03-27 11:13:52 +0800

    init code

---
+++ demodatacrawler/api.py
@@ -0,0 +1,72 @@
+'''
+Created on 2014-03-25
+@author: xiaoluBambi
+'''
+import os
+import sys
+import urllib2
+import urlparse
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def get_file_list(file_path):
+    file_list = []
+    for filepath in os.walk(file_path):
+        for file in filepath[2]:
+#             file = filepath[0] + '/' + file
+            file_list.append(file)
+    return  file_list
+
+
+USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.56 Safari/537.17"
+
+def get_html(url):
+    u = urlparse.urlparse(url)
+    referer = u.scheme + '://' + u.netloc
+    headers = {"User-Agent": USER_AGENT, "Referer": referer}
+    req = urllib2.Request(url, headers=headers)
+    response = urllib2.urlopen(req)
+    if response.code < 300:
+        result = ''
+        for line in response.readlines():
+            line = line.encode('utf-8')
+#             print line
+            result += line
+        return url, response.url, response.msg, result
+    else:
+        return None, None, None
+
+
+def save_file(file_name, *args):
+    fp = open(file_name, 'w')
+    for arg in args[0]:
+        fp.writelines(arg)
+        fp.write("\n")
+    fp.flush()
+    fp.close()
+
+
+_DYNAMIC_SCRIPT_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'download.js')
+_TIMEOUT = 30
+
+if __name__ == '__main__':
+#     browser = webdriver.Firefox()
+    browser = webdriver.Remote("http://localhost:4444/wd/hub", webdriver.DesiredCapabilities.HTMLUNIT.copy())
+    url = 'http://list.mp3.baidu.com/top/top100.html'
+    u = urlparse.urlparse(url)
+    try:
+        browser.get(url)
+        print browser.capabilities
+        print browser.find_element_by_tag_name('body').text
+        print browser.title
+    except Exception, e:
+        print e
+    finally:
+        browser.quit()
+    
+    
+#     f = get_html(url)
---
+++ demodatacrawler/main.py
@@ -0,0 +1,70 @@
+'''
+
+@author: xiaoluBambi
+'''
+import Queue
+import hashlib
+import os
+from os.path import isdir
+import random
+import threading
+import time
+import traceback
+
+from api import get_file_list, save_file, get_html
+
+
+FILE_PATH = '/home/mlliu/git/python_code/testdata/'
+HTML_FILE_PATH = '/home/mlliu/git/python_code/html'
+EMPTY_LINE = ['', '\r\n', '\r', '\n']
+THREAD_COUNT = 20
+queue = Queue.Queue(THREAD_COUNT)
+count=0
+finish_count=0
+
+def fun(path, file_name, url):
+    url = "http://" + url
+    file_name = path + '/' + file_name + '.html'
+#     print file_name
+#     print url
+    try:
+        save_file(file_name, get_html(url))
+    except Exception, e:
+        print e
+    finally:
+        print queue.qsize()
+        queue.get()
+        global finish_count
+        finish_count += 1
+        print "finished",finish_count
+
+def main():
+    file_list = get_file_list(FILE_PATH)
+
+    thread_list = []
+    for file in file_list: 
+        dir = HTML_FILE_PATH + '/' + file
+        if not isdir(dir):
+            os.mkdir(dir)
+        file = FILE_PATH + '/' + file
+        with open(file, 'r') as fp:
+            for url in fp.readlines():
+                url = url.strip()
+                if url in EMPTY_LINE:
+                    continue
+
+                file_name = str(abs(hash(url + str(time.clock())))) + str(random.random())      
+                thread_item = threading.Thread(target=fun, args=(dir, file_name , url))
+                queue.put(thread_item)
+                global count
+                count += 1
+                print "read", count
+                thread_list.append(thread_item) 
+                thread_item.start()
+    print "finished"
+    for t in thread_list:
+        t.join()
+
+
+if __name__ == '__main__':
+    main()
